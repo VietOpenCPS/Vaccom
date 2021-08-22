8c28de4bd914157717eba3ba774b9c2cd13b65a4
trungnt <trungnt.fds.vn>
2021-08-22 10:16:35 +0700

    EnableConfigurationProperties

--- backend/src/main/java/org/vaccom/vcmgt/VaccomApplication.java
+++ backend/src/main/java/org/vaccom/vcmgt/VaccomApplication.java
@@ -2,8 +2,13 @@ package org.vaccom.vcmgt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.vaccom.vcmgt.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class VaccomApplication {
 
 	public static void main(String[] args) {
