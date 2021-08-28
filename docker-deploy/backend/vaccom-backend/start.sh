eb4eafd5905ddef5ae9ce235e9507d666330d6f0
hieunguyen23 <ngvanhieu112233@gmail.com>
2021-08-28 17:45:51 +0700

    update docker deploy backend frontend

--- docker-deploy/backend/vaccom-backend/start.sh
+++ docker-deploy/backend/vaccom-backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FILE=/opt/vaccom/nohup.out
-if [! -f "$FILE"]; then
+if [ ! -f "$FILE" ]; then
 sleep 1m
 cd /opt/vaccom
 nohup /usr/bin/java -jar /opt/vaccom/backend/org.vaccom.vcmgt-1.0.0.jar -Dspring-boot.run.jvmArguments="-Duser.timezone=UTC" --spring.config.location=file:/opt/vaccom/application.properties > /opt/vaccom/nohup.out &
