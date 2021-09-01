053b52bd96c55ba7395f44af8f8d984988822048
webvang <hoang.nguyen@webvang.vn>
2021-09-01 19:41:48 +0700

    update server

--- mobile/Vaccom_Mobile/lib/model/server_info.dart
+++ mobile/Vaccom_Mobile/lib/model/server_info.dart
@@ -9,19 +9,24 @@ class ServerInfo {
     this.protocol
   });
 
-  static List<ServerInfo> all = [vaccom, mobile];
+  static List<ServerInfo> all = [vaccom, thachban, mobile];
 
   static ServerInfo vaccom = ServerInfo(
-      title: r'Vaccom',
+      title: r'Máy chủ Vaccom',
+      desc: r'HỆ THỐNG QUẢN LÝ VÀ TỔ CHỨC ĐIỂM TIÊM CHỦNG',
+      baseUrl: r'vaccom.vn:8080/rest/v1/',
+      protocol: 'https',
+      logoBanner: r'https://vaccom.vn/images/logo_banner.png');
+  static ServerInfo thachban = ServerInfo(
+      title: r'Máy chủ Phường Thạch Bàn',
       desc: r'HỆ THỐNG QUẢN LÝ VÀ TỔ CHỨC ĐIỂM TIÊM CHỦNG',
       baseUrl: r'thachban.vaccom.vn:8080/rest/v1/',
       protocol: 'https',
-      logoBanner: r'https://thachban.vaccom.vn/images/logo_banner.png');
-
+      logoBanner: r'https://vaccom.vn/images/logo_banner.png');
   static ServerInfo mobile = ServerInfo(
-      title: r'Mobile',
+      title: r'Máy chủ Mobile',
       desc: r'HỆ THỐNG QUẢN LÝ VÀ TỔ CHỨC ĐIỂM TIÊM CHỦNG',
       baseUrl: r'mobile.vaccom.vn:8080/rest/v1/',
       protocol: 'https',
-      logoBanner: r'https://thachban.vaccom.vn/images/logo_banner.png');
+      logoBanner: r'https://vaccom.vn/images/logo_banner.png');
 }
